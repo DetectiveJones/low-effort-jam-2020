f0bbb5e9aba714464855bc4725a422e81bfb12f6
Liberty <libertycgjones@gmail.com>
2020-08-26 17:04:18 +0100

    Built game.
    Completed the game.

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameScript : MonoBehaviour
+{
+   public void WinGame()
+    {
+        SceneManager.LoadScene("WinScene");
+    }
+
+    public void LoseGame()
+    {
+        SceneManager.LoadScene("LoseScene");
+    }
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
